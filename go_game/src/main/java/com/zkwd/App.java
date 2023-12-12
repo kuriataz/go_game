d8400a0b023bdde219c436fce102e94ac1ed40d5
eliwdobr <wik.2003dobr@gmail.com>
2023-12-12 18:59:22 +0100

    javafx integration (java 17)

--- go_game/src/main/java/com/zkwd/App.java
+++ go_game/src/main/java/com/zkwd/App.java
@@ -1,13 +1,29 @@
 package com.zkwd;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
- *
+ * Now with JavaFX (functional, even!)
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage){
+
+        Text txt = new Text("hello world");
+        StackPane sp = new StackPane(txt);
+
+        stage.setScene(new Scene(sp, 600, 400));
+        stage.show();
     }
 }

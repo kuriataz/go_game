5bd5c79a19da28e793be535b6b10ffec0fe4120a
eliwdobr <wik.2003dobr@gmail.com>
2023-12-13 22:25:46 +0100

    turned the GUIBoard class into a builder, and adapted it to use board strings instead for compatibility

---
+++ go_game/src/main/java/com/zkwd/client/screens/ingame/GUIBoardBuilder.java
@@ -0,0 +1,96 @@
+package com.zkwd.client.screens.ingame;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+/**
+ * Draws a board to be displayed on screen.
+ *
+ * (we could make this a builder for free tbh)
+ */
+public class GUIBoardBuilder {
+
+  /**
+   * size of circle nodes and padding - will determine size of board
+   */
+  final double CircleSize = 12;
+  final double GridPadding = 3;
+
+  Group DisplayBoard(String boardState) {
+    Group out = new Group();
+
+    String[] lines = boardState.split("|");
+
+    if(lines.length != lines[0].length()){
+      //data is wrong
+      //maybe throw something here
+      return null;
+    } 
+
+    int size = lines.length;
+
+    /**
+     * GRID LINES
+     */
+    for (int i = 0; i < size; ++i) {
+
+      Line vertical = new Line(getCoords(i, 0).getX(), 
+                               getCoords(i, 0).getY(),
+                               getCoords(i, size - 1).getX(),
+                               getCoords(i, size - 1).getY());
+
+      vertical.setStrokeWidth(2);
+      out.getChildren().add(vertical);
+
+      Line horizontal = new Line(getCoords(0, i).getX(), 
+                                 getCoords(0, i).getY(),
+                                 getCoords(size - 1, i).getX(),
+                                 getCoords(size - 1, i).getY());
+
+      horizontal.setStrokeWidth(2);
+      out.getChildren().add(horizontal);
+    }
+
+    /**
+     * STONES
+     */
+    GridPane gp = new GridPane();
+    gp.setAlignment(Pos.CENTER);
+    for (int i = 0; i < size; ++i) {
+      for (int j = 0; j < size; ++j) {
+        Circle shape = new Circle(CircleSize);
+        GridPane.setMargin(shape, new Insets(GridPadding));
+        shape.setStroke(Color.BLACK);
+        shape.setStrokeWidth(2);
+
+        switch (lines[i].charAt(j)) {
+        case 'W': // WHITE
+          shape.setFill(Color.WHITE);
+          break;
+        case 'B': // BLACK
+          shape.setFill(Color.BLACK);
+          break;
+        default:
+          shape.setFill(Color.TRANSPARENT);
+          shape.setStroke(Color.TRANSPARENT);
+        }
+        gp.add(shape, i, j);
+      }
+    }
+    out.getChildren().add(gp);
+
+    return out;
+  }
+
+  // gets coordinates in local space for the center of intersection (x, y)
+  private Point2D getCoords(int x, int y) {
+    double w = GridPadding + CircleSize + 1;
+    return new Point2D(w * (2 * x + 1), w * (2 * y + 1));
+  }
+}
--- go_game/src/main/java/com/zkwd/client/screens/lobby/LobbyScreen.java
+++ go_game/src/main/java/com/zkwd/client/screens/lobby/LobbyScreen.java
@@ -6,7 +6,6 @@ import com.zkwd.client.model.State;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;

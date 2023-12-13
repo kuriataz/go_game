190746d06738de8a552419880f2c1fd56105642a
Zofia Kuriata <zosia.kuriata7@gmail.com>
2023-12-13 17:09:45 +0100

    add setNeighbours method

--- go_game/src/main/java/com/zkwd/Board.java
+++ go_game/src/main/java/com/zkwd/Board.java
@@ -20,9 +20,9 @@ public class Board {
         new Intersection[size][size]; // by default every cell is 0 = EMPTY
   }
 
-  public int getSize() { return this.size; }
+  int getSize() { return this.size; }
 
-  public int getValue(int x, int y) { return board[x][y].getState(); }
+  int getValue(int x, int y) { return board[x][y].getState(); }
 
   void putBlack(int x, int y) {
     if (validMove(board[x][y].getState())) {
@@ -43,5 +43,24 @@ public class Board {
       return true;
     }
     return false;
+  }
+
+  void setNeighbours() {
+    for (int i = 0; i != size; ++i) {
+      for (int j = 0; j != size; ++j) {
+        if (i + 1 < size) {
+          board[i][j].neighbours.add(board[i + 1][j]);
+        }
+        if (i - 1 >= 0) {
+          board[i][j].neighbours.add(board[i - 1][j]);
+        }
+        if (j + 1 < size) {
+          board[i][j].neighbours.add(board[i][j + 1]);
+        }
+        if (j - 1 >= 0) {
+          board[i][j].neighbours.add(board[i][j - 1]);
+        }
+      }
+    }
   }
 }

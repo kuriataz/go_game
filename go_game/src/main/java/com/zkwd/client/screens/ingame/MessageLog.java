d58df0df30f287322e8c75536b735635ff8a9a16
eliwdobr <wik.2003dobr@gmail.com>
2023-12-17 19:48:27 +0100

    cosmetic change

--- go_game/src/main/java/com/zkwd/client/screens/ingame/MessageLog.java
+++ go_game/src/main/java/com/zkwd/client/screens/ingame/MessageLog.java
@@ -3,6 +3,7 @@ package com.zkwd.client.screens.ingame;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class MessageLog extends VBox{
 
@@ -19,8 +20,14 @@ public class MessageLog extends VBox{
         this.getChildren().add(lbl);
 
         // pop oldest child if necessary
-        if(this.getChildren().size() >= length){
+        if(this.getChildren().size() > length){
             this.getChildren().remove(0);
         }
+
+        for(int i = 0; i < this.getChildren().size(); ++i){
+            Label l = ((Label) this.getChildren().get(i));
+            l.setTextFill(new Color(0, 0, 0, Math.min(1.0, (i + 7.0 - this.getChildren().size()) / 5.0)));
+        }
     }
+
 }

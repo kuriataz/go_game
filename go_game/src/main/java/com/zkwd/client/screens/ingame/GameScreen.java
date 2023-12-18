5eaa3890eb4cb83884280b87d6816fbba7e46e20
Zofia Kuriata <zosia.kuriata7@gmail.com>
2023-12-18 08:31:28 +0100

    some cleaning

--- go_game/src/main/java/com/zkwd/client/screens/ingame/GameScreen.java
+++ go_game/src/main/java/com/zkwd/client/screens/ingame/GameScreen.java
@@ -63,101 +63,42 @@ public class GameScreen extends BorderPane implements IScreen {
           // wait for your round
           do {
             message = App.await();
+            Platform.runLater(() -> { disableInput(); });
           } while (!message.equals("game_go"));
 
           String verdict;
           // take inputs until server decides input is correct
           do {
-            // mouse not disabled
+            Platform.runLater(() -> { enableInput(); });
 
             verdict = App.await();
           } while (!verdict.equals("game_correct"));
 
-          // update board state
           boardString = App.await();
 
-          board = boardBuilder.DisplayBoard(boardString);
-
-          // Platform.runLater(() -> {
-          //   log.push(boardString);
-          // });
+          Platform.runLater(() -> {
+            updateBoard(boardString);
+            // log.push(boardString);
+          });
         }
-
-        // String message;
-        // String s = App.await();
-
-        // String clr = s.split("_")[1];
-        // String brd = s.split("_")[2];
-
-        // Platform.runLater(() -> {
-        //   updateBoard(brd);
-        //   txt.setText(clr);
-        // });
-
-        // while(true){
-        //   // wait for your round
-        //   do {
-        //     message = App.await();
-        //   } while(!message.equals("game_go") &&
-        //   !message.equals("game_goagain"));
-
-        //   if(message.equals("game_go")){
-
-        //     // next signal will be game_[round]_[boardState]
-        //     String nboard = App.await();
-        //     String[] split = nboard.split("_");
-
-        //     Platform.runLater(() -> {
-        //       // TODO : update round with split[1]
-        //       updateBoard(split[2]);
-        //     });
-        //   }
-
-        //   Platform.runLater(() -> {
-        //     enableInput();
-        //   });
-
-        //   /**
-        //    * App proceeds to the next loop iteration and does nothing until
-        //    it receives a signal to move again ("game_go"
-        //    * or "game_goagain")
-        //    * In that time, a message is only sent to the server if the player
-        //    clicks on the board.
-        //    * When the player clicks on the board, the app requests to make
-        //    the move and waits for the server to
-        //    * tell it whether the move is valid or not.
-        //    * If the move is valid, the server moves on to ask the other
-        //    player.
-        //    * Otherwise, the server gives a "game_goagain" signal to the app,
-        //    which causes it to move again,
-        //    * but without updating the board this time.
-        //    *
-        //    * Also, all messages received while waiting for "game_go" or
-        //    "go_again" are ignored.
-        //    *
-        //    * I Am Not Sure This Works Lol
-        //    */
-        // }
       }
     }.start();
 
     return true;
   }
 
-  // private void updateBoard(String boardString){
-  //   board = boardBuilder.DisplayBoard(boardString);
-  //   this.setCenter(board);
-  // }
+  private void updateBoard(String boardString) {
+    board = boardBuilder.DisplayBoard(boardString);
+    this.setCenter(board);
+  }
 
-  // private void enableInput() {
-  //   System.out.println("awaiting user input");
-  //   board.setOnMouseClicked(clickHandler);
-  // }
+  private void enableInput() {
+    System.out.println("awaiting user input");
+    board.setOnMouseClicked(clickHandler);
+  }
 
-  // // not sure this is the proper way to do this
-  // private void disableInput() {
-  //   board.setOnMouseClicked(null);
-  // }
+  // not sure this is the proper way to do this
+  private void disableInput() { board.setOnMouseClicked(null); }
 
   /**
    * X and Y are in local space, so the handler should be applied to the entire
@@ -174,11 +115,9 @@ public class GameScreen extends BorderPane implements IScreen {
     int clickedX = (int)((mouseX - gridPadding) / (2 * circleSize + 1));
     int clickedY = (int)((mouseY - gridPadding) / (2 * circleSize + 1));
 
-    // Convert the coordinates to a string format and transmit it
+    // Convert the coordinates to a string format and send it
     String clickedPosition = clickedX + " " + clickedY;
 
-    // ask if move is correct
-    // String result = App.transmit("move:" + clickedPosition);
     App.send(clickedPosition);
   };
 
--- go_game/src/main/java/com/zkwd/server/game/GoGame.java
+++ go_game/src/main/java/com/zkwd/server/game/GoGame.java
@@ -98,74 +98,9 @@ public class GoGame {
         currentPlayer = black;
         otherPlayer = white;
       }
-
-      // // player makes move or passes, and sends that here
-      // currentPlayer.send("game_go");
-      // currentPlayer.send("game_" + round + "_" + board.prepareBoardString());
-      // String move = currentPlayer.await();
-      // System.out.println("game received " + move);
-
-      // // pass
-      // if (move.equals("move:pass")) {
-      //   System.out.println("1." + move);
-
-      //   // if player passes, change the turn and go to next loop
-      //   if (currentPlayer == black) {
-      //     currentPlayer = white;
-      //   } else {
-      //     currentPlayer = black;
-      //   }
-      // } else {
-      //   System.out.println("1." + move);
-      //   // move format is "move:x y"
-      //   move = move.substring("move:".length());
-      //   System.out.println("2." + move);
-
-      //   try {
-      //     int x = Integer.parseInt(move.split(" ")[0]);
-      //     int y = Integer.parseInt(move.split(" ")[1]);
-
-      //     // check move for correctness
-      //     boolean correct = board.correctMove(x, y, turn);
-
-      //     if (correct) {
-      //       System.out.println("this is a test");
-
-      //       if (currentPlayer == black) {
-      //         board.putBlack(x, y);
-      //         currentPlayer = white;
-      //         turn = WHITE;
-      //       } else {
-      //         board.putWhite(x, y);
-      //         currentPlayer = black;
-      //         turn = BLACK;
-      //       }
-
-      //       // send correct signal: being the new board to display
-      //       String newBoard = board.prepareBoardString();
-      //       white.send("game_" + round + "_" + newBoard);
-      //       black.send("game_" + round + "_" + newBoard);
-
-      //       if(turn == WHITE){
-      //         round++;
-      //       }
-      //     } else {
-      //       // send incorrect signal - current player must go again
-      //       currentPlayer.send("_game_incorrect");
-      //       currentPlayer.send("game_goagain");
-      //     }
-      //   } catch (NumberFormatException e) {
-      //     // the transmitted move was somehow incorrect - current player must
-      //     // try again
-      //     currentPlayer.send("game_goagain");
-      //   }
-      // }
     }
   }
 
-  // private boolean checkValidity(String input){
-  //   return !input.equals("12345");
-  // }
   private int[] splitMove(String clickedPosition) {
     int[] coordinates = new int[2];
 
@@ -182,8 +117,7 @@ public class GoGame {
       coordinates[1] = y;
 
     } catch (NumberFormatException e) {
-      // The transmitted move was somehow incorrect - current player must try
-      // again You might want to log or print an error message here
+      // The transmitted move was incorrect - current player must try again
     }
 
     return coordinates;

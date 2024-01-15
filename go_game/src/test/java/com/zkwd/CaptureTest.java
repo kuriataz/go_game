4a7c66470246a3af109a8777877838b151bdc613
Zofia Kuriata <zosia.kuriata7@gmail.com>
2024-01-15 10:08:12 +0100

    modify captureTest still not working

--- go_game/src/test/java/com/zkwd/CaptureTest.java
+++ go_game/src/test/java/com/zkwd/CaptureTest.java
@@ -1,47 +1,55 @@
-// package com.zkwd;
+package com.zkwd;
 
-// import com.zkwd.server.game.exceptions.MoveException;
-// import com.zkwd.server.game.gamestate.Board;
+import com.zkwd.server.game.exceptions.MoveException;
+import com.zkwd.server.game.gamestate.Board;
 
-// public class CaptureTest {
-//   public void testCaptureStone() {
-//     Board b = new Board(9);
-//     b = b.setBoard("EWEEEEEBW|EWEEEEEEE|EEEEEEEEE|EEEEEEEEE|EEEEBEEEE|"
-//                    + "EEEBWBEEE|EEEEEEEEE|EEEEEEEEE|EEEEEEEEE|");
+// TODO: find out why it doesn't work
 
-//     try {
-//       b.putStone(1, 8, -1);
-//     } catch (MoveException e) {
-//       e.printStackTrace();
-//     }
+public class CaptureTest {
+  public void testCaptureStone() {
+    Board b = new Board(9);
+    b = b.setBoard("BEEEEEEEE|BEEEEEEEE|EEEEEEEEE|EEEEEEEEE|EEEEWEEEE|"
+                   + "EEEWBWEEE|EEEEEEEEE|BEEEEEEEE|WEEEEEEEE|");
 
-//     assert (b.board[0][8].getState() == 0);
-//     try {
-//       b.putStone(6, 4, -1);
-//     } catch (MoveException e) {
-//       e.printStackTrace();
-//     }
-//     assert (b.board[5][4].getState() == 0);
-//   }
-//   public void testCaptureChain() {
-//     Board b = new Board(9);
-//     b = b.setBoard("BWEEEEEBE|BWEEEEEEB|EWEEEBEEE|EEEEBWBEE|EEEEBWEEE|" +
-//                    "EEEBEBEEE|WEEEBEEEE|WEEEEEEEE|WEEEEEEEE|");
+    try {
+      b.putStone(8, 1, -1);
+      b.removeCapturedStones();
+    } catch (MoveException e) {
+      e.printStackTrace();
+    }
+    String newString = b.prepareBoardString();
+    System.out.println(newString);
 
-//     try {
-//       b.putStone(0, 2, 1);
-//     } catch (MoveException e) {
-//       e.printStackTrace();
-//     }
+    // assert (b.board[8][0].getState() == 0);
+    try {
+      b.putStone(6, 4, 1);
+      b.removeCapturedStones();
+    } catch (MoveException e) {
+      e.printStackTrace();
+    }
+    // assert (b.board[5][4].getState() == 0);
+  }
+  public void testCaptureChain() {
+    Board b = new Board(9);
+    b = b.setBoard("BWEEEEEBE|BWEEEEEEB|EWEEEBEEE|EEEEBWBEE|EEEEBWEEE|"
+                   + "EEEBEBEEE|WEEEBEEEE|WEEEEEEEE|WEEEEEEEE|");
 
-//     assert (b.board[0][0].getState() == 0);
-//     assert (b.board[0][1].getState() == 0);
-//     try {
-//       b.putStone(4, 6, -1);
-//     } catch (MoveException e) {
-//       e.printStackTrace();
-//     }
-//     assert (b.board[4][5].getState() == 0);
-//     assert (b.board[3][5].getState() == 0);
-//   }
-// }
+    try {
+      b.putStone(0, 2, 1);
+      b.removeCapturedChains();
+    } catch (MoveException e) {
+      e.printStackTrace();
+    }
+
+    // assert (b.board[0][0].getState() == 0);
+    // assert (b.board[0][1].getState() == 0);
+    try {
+      b.putStone(4, 6, -1);
+      b.removeCapturedChains();
+    } catch (MoveException e) {
+      e.printStackTrace();
+    }
+    // assert (b.board[4][5].getState() == 0);
+    // assert (b.board[3][5].getState() == 0);
+  }
+}
